4e539ec11d75fed999a56bdba229b392d232c2d0
Diwakar Reddy Gudisi <diwakarreddy45@gmail.com>
2017-12-13 00:08:05 +0800

    logging is added

--- src/main/java/com/diwakar/rest/SpringRestDemo/BooksController.java
+++ src/main/java/com/diwakar/rest/SpringRestDemo/BooksController.java
@@ -1,6 +1,7 @@
 package com.diwakar.rest.SpringRestDemo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,6 @@ public class BooksController {
 
     @GetMapping("/books")
     public List<Book> getList(){
-        return new ArrayList<Book>();
+        return Arrays.asList(new Book(1,"D", "diwakar"));
     }
 }
